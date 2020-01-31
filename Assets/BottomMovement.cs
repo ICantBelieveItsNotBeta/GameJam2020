433569cb87234d3b44e472cd2bb87d90e9dc8e04
jembi <jembishop@gmail.com>
2020-01-31 19:18:16 +0000

    basic movement

---
+++ Assets/BottomMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BottomMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float distToGround = 0;
+    public float jumpforce;
+    public float runspeed;
+    public float groundFriction;
+    public float accel;
+    public float groundFrictionSpeedMult;
+    bool jumping;
+    float grav;
+    public Feet feet;
+    Rigidbody2D rb;
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        distToGround = GetComponent<Collider2D>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var jump = Input.GetAxisRaw("Jump") == 1;
+        print(jump);
+        if (jump && IsGrounded())
+        {
+            if (!jumping)
+            {
+                rb.velocity += Vector2.up * jumpforce;
+                jumping = true;
+            }
+        }
+        if (!jump)
+        {
+            jumping = false;
+        }
+    }
+    bool IsGrounded()
+    {
+        //print(feet.colliding);
+        return feet.colliding;
+    }
+    private void FixedUpdate()
+    {
+        var dir = Input.GetAxisRaw("Horizontal");
+        rb.AddForce(Vector2.right * accel * dir);
+        if (rb.velocity.x > runspeed)
+        {
+            rb.velocity = new Vector2(runspeed, rb.velocity.y);
+        }
+        else if (rb.velocity.x < -runspeed)
+        {
+            rb.velocity = new Vector2(-runspeed, rb.velocity.y);
+        }
+        print(dir);
+        if (IsGrounded())
+        {
+            rb.AddForce(-Vector2.right * (float)Math.Tanh(groundFrictionSpeedMult * rb.velocity.x) * groundFriction);
+        }
+    }
+}
---
+++ Assets/Feet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool colliding;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+
+            colliding = true;
+
+
+        }
+
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+
+            colliding = false;
+
+
+        }
+
+
+    }
+
+}
