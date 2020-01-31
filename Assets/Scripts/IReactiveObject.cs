63142af942685bb234bbafeee047c13b8099032e
Ed Jeffreys <ed@edjeffreys.com>
2020-01-31 19:47:28 +0000

    create PressureTrigger and IReactiveObject

---
+++ Assets/Scripts/IReactiveObject.cs
@@ -0,0 +1,5 @@
+﻿public interface IReactiveObject
+{
+    void Activate();
+    void Deactivate();
+}
---
+++ Assets/Scripts/PressureTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PressureTrigger : MonoBehaviour
+{
+    public IReactiveObject reactiveObject;
+
+    private Rigidbody2D _trigger;
+
+    private void Awake()
+    {
+        _trigger = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.rigidbody.position.x > _trigger.position.x)
+            reactiveObject.Activate();
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        reactiveObject.Deactivate();
+    }
+}
