3ae53966fc1c4154b3afada34bc05d15f4df54f5
Memymoi <34973237+Memymoi@users.noreply.github.com>
2020-02-01 14:55:55 +0000

    Update Water.cs

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -2,13 +2,11 @@
 
 public class Water : MonoBehaviour
 {
-    private WorldManager _worldManager;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            _worldManager.Kill();
+            WorldManager.worldManager.Kill();
         }
     }
 }
