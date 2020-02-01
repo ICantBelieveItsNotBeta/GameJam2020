40eb002f54310f2fd44a1d12fed98526a70f59d2
Jonny Spicer <spicerjonny@gmail.com>
2020-02-01 20:07:18 +0000

    Add new hazard prefab and level 9

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+
+    private Vector3 _startPosition;
+    // public float speed = 0.01f;
+    public GameObject wall;
+    public float speed;
+    public bool clockwise;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            WorldManager.worldManager.Kill();
+        }
+    }
+
+    private void Update()
+    {
+        transform.RotateAround(wall.transform.position, (clockwise == true) ? Vector3.forward : Vector3.back, (360 - (360/speed)) * Time.deltaTime);
+    }
+}
