120933c285781a265bc54bc57955f4f8f53b62aa
Memymoi <34973237+Memymoi@users.noreply.github.com>
2020-02-01 11:41:56 +0000

    Laser Emitter + Sensor

---
+++ Assets/Scripts/LaserEmitter.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEmitter : ReactiveObject
+{
+    public bool isOn = true;
+
+    LineRenderer lr;
+
+    LaserSensor lastSensorHit;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+
+        isOn = true;
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        isOn = false;
+    }
+
+    private void Update()
+    {
+        if (isOn) ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(this.transform.position, this.transform.right);
+
+        // If it hits something...
+        if (hitInfo.collider != null)
+        {
+            GameObject hitObj = hitInfo.collider.gameObject;
+
+            lr.SetPosition(0, this.transform.position);
+            lr.SetPosition(1, hitInfo.point);
+
+            if(hitObj.tag == "Player") { WorldManager.worldManager.Kill(); return; }
+
+            if (hitObj.GetComponent<LaserSensor>() != null)
+            {
+                if (lastSensorHit != null && lastSensorHit != (hitObj.GetComponent<LaserSensor>()))
+                {
+                    lastSensorHit.Deactivate();
+                    lastSensorHit = null;
+                }
+
+                lastSensorHit = hitObj.GetComponent<LaserSensor>();
+                lastSensorHit.Activate();
+            }
+            else if(lastSensorHit != null)
+            {
+                lastSensorHit.Deactivate();
+                lastSensorHit = null;
+            }
+        }
+        else
+        {
+            lr.SetPosition(0, this.transform.position);
+            lr.SetPosition(1, this.transform.position);
+
+            if (lastSensorHit != null)
+            {
+                lastSensorHit.Deactivate();
+                lastSensorHit = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LaserSensor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSensor : TriggerBase
+{
+    
+
+    public void Activate()
+    {
+        foreach (ReactiveObject r in reactiveObjects) r.Activate();
+    }
+
+    public void Deactivate()
+    {
+        foreach (ReactiveObject r in reactiveObjects) r.Deactivate();
+    }
+}
